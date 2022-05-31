dc419b1d29d45fca9b2071621a771aa82fa64fbe
OwenPortfolio <owen.manby@gmail.com>
2022-05-31 18:18:45 +0100

    further logic on switching off lights

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,43 +1,26 @@
 import Box from '../components/Box';
-import {useState} from 'react';
 
 const Board = () => {
     
-    const [id, setId] = useState('');
-    let fixId = (event) => {
-        setId(event.target.parentElement.id)
-        clickTile(id)
-    }
-    const clickTile = (id) => {
-        console.log(id)
-        const neighbours = [(id[0] + (id[1] - 1))]
-        const neighbour = document.getElementById(neighbours[0]);
-        const light = neighbour.children[0];
-        light.click();
-    }
-    
     return <section class="gameBoard">
         <div class="grid-container">
-            <div class="tile" id="A1" onClick={fixId}><Box/></div>
-            <div class="tile" id="A2" onClick={fixId}><Box/></div>
-            <div class="tile" id="A3" onClick={fixId}><Box/></div>
-            <div class="tile" id="A4" onClick={fixId}><Box/></div>
-            <div class="tile" id="B1" onClick={fixId}><Box/></div>
-            <div class="tile" id="B2" onClick={fixId}><Box/></div>
-            <div class="tile" id="B3" onClick={fixId}><Box/></div>
-            <div class="tile" id="B4" onClick={fixId}><Box/></div>
-            <div class="tile" id="C1" onClick={fixId}><Box/></div>
-            <div class="tile" id="C2" onClick={fixId}><Box/></div>
-            <div class="tile" id="C3" onClick={fixId}><Box/></div>
-            <div class="tile" id="C4" onClick={fixId}><Box/></div>
-            <div class="tile" id="D1" onClick={fixId}><Box/></div>
-            <div class="tile" id="D2" onClick={fixId}><Box/></div>
-            <div class="tile" id="D3" onClick={fixId}><Box/></div>
-            <div class="tile" id="D4" onClick={fixId}><Box/></div>
-
+            <div class="tile" id="A1"><Box/></div>
+            <div class="tile" id="A2"><Box/></div>
+            <div class="tile" id="A3"><Box/></div>
+            <div class="tile" id="A4"><Box/></div>
+            <div class="tile" id="B1"><Box/></div>
+            <div class="tile" id="B2"><Box/></div>
+            <div class="tile" id="B3"><Box/></div>
+            <div class="tile" id="B4"><Box/></div>
+            <div class="tile" id="C1"><Box/></div>
+            <div class="tile" id="C2"><Box/></div>
+            <div class="tile" id="C3"><Box/></div>
+            <div class="tile" id="C4"><Box/></div>
+            <div class="tile" id="D1"><Box/></div>
+            <div class="tile" id="D2"><Box/></div>
+            <div class="tile" id="D3"><Box/></div>
+            <div class="tile" id="D4"><Box/></div>
         </div>
-
-
     </section>
 }
 
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,10 +1,22 @@
 import {useState} from 'react';
 
 const Box = () => {
-
     const[isOn, setIsOn] = useState('false');
-    const lightswitch = () => {
-        setIsOn(current => !current);        
+
+    const lightswitch = (event) => {
+        setIsOn(current => !current);
+        const id = event.target.parentElement.id;
+        console.log(id)
+        neighbour(id)
+        }
+    const[count, setCount] = useState(0);
+
+    const neighbour = (id) => {
+        let neighbourId = id[0] + (id[1] -1)
+        let light = document.getElementById(neighbourId).children[0]
+
+        console.log(document.getElementById(id).children)
+        light.click();
     }
     return <div class="light" style={{backgroundColor: isOn ? '#21e171' : ''}} onClick={lightswitch}></div>
 }
