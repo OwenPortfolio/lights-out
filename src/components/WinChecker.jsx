279b30e89cc8a00d769a04aff52e5c1572cc0bbd
OwenPortfolio <owen.manby@gmail.com>
2022-06-03 15:17:02 +0100

    update win checker

--- src/components/WinChecker.jsx
+++ src/components/WinChecker.jsx
@@ -3,7 +3,8 @@ const WinChecker = () => {
     let grid = ['A1', 'A2', 'A3', 'A4',
     'B1', 'B2', 'B3', 'B4',
     'C1', 'C2', 'C3', 'C4',
-    'D1', 'D2', 'D3', 'D4']
+    'D1', 'D2', 'D3', 'D4',
+    'E1', 'E2', 'E3', 'E4']
 
     const checkWins = () => {
         let lights = 0;
@@ -13,7 +14,7 @@ const WinChecker = () => {
             if(tile.style[0] !== 'background-color'){
                 lights++;
             }
-            if(lights === 16){
+            if(lights === 25){
                 document.getElementById('NewGameButton').click();
                 alert('You Win!')
             }
