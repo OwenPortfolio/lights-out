ae4fe1567478a2c9980c1d8477ecf74851470ee3
OwenPortfolio <owen.manby@gmail.com>
2022-06-03 16:39:29 +0100

    improved header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ const Header = () => {
     return <header class="App-Header">
         <title>Lights Owen</title>
         <h1>Lights Owen</h1>
+        <h2>Turn out all the lights to win.</h2>
         </header>
 }
 
